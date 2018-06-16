dd8293aa7835237731a99f9ddeb95bf9fdb68e8c
rednjzz <rednjzz@gmail.com>
2018-06-17 00:17:10 +0900

    Test Back_End api Router Setup with favicon

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let favicon = require('serve-favicon');
 
 let app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,8 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'fe', 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +30,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({success: false, msg: err.message});
 });
 
 module.exports = app;

a7589b4b1b9ce206789290b51b5e97115ba5deb6
rednjzz <rednjzz@gmail.com>
2018-06-17 13:18:32 +0900

    Back-End Api Test and Api Router Test

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const test = require('./test');
+const data = require('./data');
 
 router.all('*', (req, res, next) => {
     console.log(req.path + 'welcom to api');
@@ -8,6 +9,7 @@ router.all('*', (req, res, next) => {
 });
 
 router.use('/test', test);
+router.use('/data', data);
 
 router.all('*', (req, res) => {
     res.status(404).send({success: false, msg: `unknown uri ${req.path}`});
